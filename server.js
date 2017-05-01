838ee753117512ed3f9eab99f76d230a26142dcd
wagnersouz4 <wagnersouz4@gmail.com>
2017-05-01 01:33:00 -0300

    updating server.js

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
-let express = require('express');
-let app = express();
-app.use('/', express.static(__dirname + '/public'));
+let express = require('express'),
+    app = express(),
+    path = require('path');
+
+app.use(express.static(path.join(__dirname + '/public')));
+app.use('/assets', express.static(__dirname + '/public/assets'));
+app.use('/resources', express.static(__dirname + '/public/resources'));
+
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname + '/public/index.html'));
+});
 
 let port = process.env.PORT || 8080;
+
 app.listen(port, function() {
-    console.log("application running");
+    console.log("application running at 0.0.0.0:" + port);
 });
 
 
